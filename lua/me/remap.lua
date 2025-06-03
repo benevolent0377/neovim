df4be7d5f306e9046abe660d8d186fb53e763f37
calithos4136 <149543612+calithos4136@users.noreply.github.com>
2025-06-03 16:11:53 -0500

    created default config files

---
+++ lua/me/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
+vim.keymap.set("n", "<leader>;;", vim.cmd.terminal)
+vim.keymap.set("n", "<C-u>", vim.cmd.PackerSync)
+vim.keymap.set("n", "<leader>cl", vim.cmd.close)
