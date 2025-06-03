1a02e9861b0139091a31ed0690e8d0c585f6623e
calithos4136 <149543612+calithos4136@users.noreply.github.com>
2025-06-03 18:17:58 -0500

    added color change functions in colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 
-function ColorMe(color)
+function NormalMode(color)
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
@@ -8,4 +8,22 @@ function ColorMe(color)
 
 end
 
-ColorMe()
+
+function MorningMode(color)
+	color = color or "habamax"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'NormalFloat', { bg= 'none' })
+
+end
+
+
+function BIOSMode(color)
+	color = color or "darkblue"
+	vim.cmd.colorscheme(color)
+
+	--vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+	--vim.api.nvim_set_hl(0, 'NormalFloat', { bg= 'none' })
+
+end
